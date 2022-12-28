aad0eb046548c7e616887c0ffe96dc66686a6ed4
Surya T N <suryatej.nim@yahoo.com>
2022-12-29 00:14:20 +0530

    refactor: removed appwrite configuration code
    Removed appwrite initial configuration code
    as part of debugging issue with accessing

--- src/appwrite-api/index.js
+++ src/appwrite-api/index.js
@@ -1,8 +1,1 @@
-import { Client } from "appwrite";
 
-// Appwrite integration
-const client = new Client();
-
-client
-  .setEndpoint(process.env.REACT_APP_APPWRITE_CLIENT_ENDPOINT)
-  .setProject(process.env.REACT_APP_APPWRITE_PROJECT_ID);
