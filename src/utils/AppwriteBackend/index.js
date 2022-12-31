cd50030d98ca9ced21b7337240a07938e1d2ec20
Surya T N <suryatej.nim@yahoo.com>
2022-12-31 18:11:25 +0530

    chore: updated appwrite configuration
    Updated the environment variables used to
    setup the Appwrite configuration, like,
    database collections, storage buckets, to
    be used in other parts of the app in carrying
    out necessary backend operations

--- src/utils/AppwriteBackend/index.js
+++ src/utils/AppwriteBackend/index.js
@@ -2,14 +2,6 @@ export const AppwriteBackend = {
   clientEndpoint: process.env.REACT_APP_APPWRITE_CLIENT_ENDPOINT,
   projectId: process.env.REACT_APP_APPWRITE_PROJECT_ID,
   databaseId: process.env.REACT_APP_APPWRITE_DATABASE_ID,
-  brandCollectionId: process.env.REACT_APP_APPWRITE_BRAND_COLLECTION_ID,
-  storeCollectionId: process.env.REACT_APP_APPWRITE_STORE_COLLECTION_ID,
-  productCollectionId: process.env.REACT_APP_APPWRITE_PRODUCT_COLLECTION_ID,
-  productDataForAnalysisAndVerificationCollectionId:
-    process.env
-      .REACT_APP_APPWRITE_PRODUCT_DATA_FOR_ANALYSIS_AND_VERIFICATION_COLLECTION_ID,
-  brandImagesBucketId: process.env.REACT_APP_APPWRITE_BRAND_IMAGES_BUCKET_ID,
-  storeImagesBucketId: process.env.REACT_APP_APPWRITE_STORE_IMAGES_BUCKET_ID,
-  productImagesBucketId:
-    process.env.REACT_APP_APPWRITE_PRODUCT_IMAGES_BUCKET_ID,
+  databaseCollections: process.env.REACT_APP_APPWRITE_COLLECTIONS,
+  storageBuckets: process.env.REACT_APP_APPWRITE_BUCKETS,
 };
