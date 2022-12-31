fb92c3c3a12db8a0a0f0c908014d13f9d53a14b0
Surya T N <suryatej.nim@yahoo.com>
2022-12-31 22:07:03 +0530

    feat(appwrite-backend): implemented signup, login api flows
    Implemented signup and login methods to take
    in user data, credentials and process them
    through Appwrite login, signup end-points
    by creating new account and mapping new
    email session to the same

--- src/appwrite-api/index.js
+++ src/appwrite-api/index.js
@@ -1,1 +1,37 @@
+import { Client, Account } from "appwrite";
+import AppwriteBackend from "../utils/AppwriteBackend";
 
+const client = new Client();
+const account = new Account(client);
+
+const { clientEndpoint, projectId } = AppwriteBackend;
+
+client.setEndPoint(clientEndpoint).setProject(projectId);
+
+export const userSignup = async (userData) => {
+  const { userName, userEmail, userPassword } = userData;
+
+  let signupResponse = null;
+
+  try {
+    signupResponse = await account.create(userName, userEmail, userPassword);
+  } catch (signupError) {
+    signupResponse = signupError;
+  }
+
+  return signupResponse;
+};
+
+export const userLogin = async (userCredentials) => {
+  const { userEmail, userPassword } = userCredentials;
+
+  let loginResponse = null;
+
+  try {
+    loginResponse = await account.createEmailSession(userEmail, userPassword);
+  } catch (loginError) {
+    loginResponse = loginError;
+  }
+
+  return loginResponse;
+};
