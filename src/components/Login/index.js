040e83cb07cf7e79e9aedb287d5ad70a6df17ccd
Surya T N <suryatej.nim@yahoo.com>
2023-01-02 01:11:14 +0530

    feat(login): added authentication state management
    Imported react-loader-spinner, js-cookie npm
    packages to store auth token and handle
    authentication processing state.
    
    Additional states include: initial, success,
    failure, according to which UI is updated to
    indicate state change to the user.
    
    Still bare functionality that needs further
    work and code refactoring.

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import { Navigate } from "react-router-dom";
 import { IconContext } from "react-icons";
 import { BsFillEyeSlashFill, BsFillEyeFill } from "react-icons/bs";
+import Cookies from "js-cookie";
+import { Oval } from "react-loader-spinner";
 
 import { userSignup, userLogin } from "../../appwrite-api";
 
@@ -36,6 +39,18 @@ const Login = (props) => {
 
   const [showPassword, toggleShowPassword] = useState(false);
   const [isLogin, setIsLogin] = useState(true);
+  const [authStatus, setAuthStatus] = useState("initial");
+  const [isInitialLoad, setIsInitialLoad] = useState("true");
+  let isUserLoggedIn = useRef(false);
+
+  useEffect(() => {
+    const authToken = Cookies.get("did-it-shrink-jwt-token");
+    if (authToken !== undefined) {
+      isUserLoggedIn.current = true;
+    }
+
+    setIsInitialLoad(false);
+  }, [authStatus]);
 
   // Form user input change event handler
   // to access updated form user input and
@@ -63,131 +78,205 @@ const Login = (props) => {
     formSubmitEvent.preventDefault();
     console.log(formUserInput);
 
+    setAuthStatus("loading");
+
     if (isLogin) {
       userLogin(formUserInput)
-        .then((loginResponse) => loginResponse.json())
-        .then((jsonResponse) => console.log(`Login response: ${jsonResponse}`));
+        .then((loginResponse) => {
+          console.log(`Login response: ${loginResponse}`);
+          console.log(
+            `Login response access token: ${loginResponse.providerAccessToken}`
+          );
+          for (let responseObjKey in loginResponse) {
+            console.log(
+              `responseObjKey: ${responseObjKey}, responseObjValue: ${loginResponse[responseObjKey]}`
+            );
+          }
+
+          setAuthStatus("success");
+        })
+        .catch((loginError) => {
+          console.log(loginError);
+          setAuthStatus("failure");
+        });
     } else {
       userSignup(formUserInput)
-        .then((signupResponse) => signupResponse.json())
-        .then((jsonResponse) =>
-          console.log(`Signup response: ${jsonResponse}`)
-        );
+        .then((signupResponse) => {
+          console.log(`Signup response: ${signupResponse}`);
+          return userLogin(formUserInput)
+            .then((loginResponse) => {
+              console.log(`Login response: ${loginResponse}`);
+              console.log(
+                `Login response access token: ${loginResponse.providerAccessToken}`
+              );
+              setAuthStatus("success");
+            })
+            .catch((loginError) => {
+              console.log(loginError);
+              setAuthStatus("failure");
+            });
+        })
+        .catch((signupError) => {
+          console.log(signupError);
+          setAuthStatus("failure");
+        });
     }
   };
 
   const toggleSignup = () =>
     setIsLogin((previousIsLoginState) => !previousIsLoginState);
 
-  return (
-    <LoginBgContainer>
-      <LoginContentContainer>
-        <LoginBrandContentContainer>
-          <BrandName>
-            Did It <BrandNamePartial as="span">Shrink</BrandNamePartial>
-          </BrandName>
-          <GreyTextBig>
-            A crowdsourcing platform for product data and discovering trends
-            like Shrinkflation, Brand Pricing Strategies, and more.
-          </GreyTextBig>
-          <br />
-          <GreyTextMedium>
-            Join to contribute and leverage the collective effort.
-          </GreyTextMedium>
-        </LoginBrandContentContainer>
+  const getSubmitButtonText = () => {
+    let buttonText = "";
+    if (authStatus === "initial") {
+      buttonText = "Submit";
+    } else if (authStatus === "loading") {
+      buttonText = "Processing";
+    } else if (authStatus === "success") {
+      buttonText = `${isLogin ? "Login" : "Signup"} Success`;
+    } else if (authStatus === "failure") {
+      buttonText = `${isLogin ? "Login" : "Signup"} Failure`;
+    }
 
-        <LoginFormContentContainer>
-          <LoginFormHeader>{isLogin ? "Login" : "Signup"}</LoginFormHeader>
-          <LoginFormContainer onSubmit={onFormSubmit}>
-            <LoginFormInputsContainer>
-              {!isLogin && (
+    return buttonText;
+  };
+
+  let renderedFinalUI = null;
+  if (isInitialLoad) {
+    renderedFinalUI = (
+      <Oval
+        height={80}
+        width={80}
+        color="#eacd3c"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        ariaLabel="oval-loading"
+        secondaryColor="#f5e17e"
+        strokeWidth={2}
+        strokeWidthSecondary={2}
+      />
+    );
+  } else if (isUserLoggedIn.current) {
+    renderedFinalUI = <Navigate to="/" replace />;
+  } else {
+    renderedFinalUI = (
+      <LoginBgContainer>
+        <LoginContentContainer>
+          <LoginBrandContentContainer>
+            <BrandName>
+              Did It <BrandNamePartial as="span">Shrink</BrandNamePartial>
+            </BrandName>
+            <GreyTextBig>
+              A crowdsourcing platform for product data and discovering trends
+              like Shrinkflation, Brand Pricing Strategies, and more.
+            </GreyTextBig>
+            <br />
+            <GreyTextMedium>
+              Join to contribute and leverage the collective effort.
+            </GreyTextMedium>
+          </LoginBrandContentContainer>
+
+          <LoginFormContentContainer>
+            <LoginFormHeader>{isLogin ? "Login" : "Signup"}</LoginFormHeader>
+            <LoginFormContainer onSubmit={onFormSubmit}>
+              <LoginFormInputsContainer>
+                {!isLogin && (
+                  <LoginFormSingleInputContainer>
+                    <GreyTextMedium as="label" htmlFor="userName" isLabel>
+                      Username
+                    </GreyTextMedium>
+                    <UserInputContainer>
+                      <LoginFormInput
+                        id="userName"
+                        type="text"
+                        name="userName"
+                        value={formUserInput.userName}
+                        placeholder="Type in username..."
+                        onChange={onFormUserInputChange}
+                      />
+                    </UserInputContainer>
+                  </LoginFormSingleInputContainer>
+                )}
                 <LoginFormSingleInputContainer>
-                  <GreyTextMedium as="label" htmlFor="userName" isLabel>
-                    Username
+                  <GreyTextMedium as="label" htmlFor="userEmail" isLabel>
+                    Email
                   </GreyTextMedium>
                   <UserInputContainer>
                     <LoginFormInput
-                      id="userName"
-                      type="text"
-                      name="userName"
-                      value={formUserInput.userName}
-                      placeholder="Type in username..."
+                      id="userEmail"
+                      type="email"
+                      name="userEmail"
+                      value={formUserInput.userEmail}
+                      placeholder="Enter your valid email address"
                       onChange={onFormUserInputChange}
                     />
                   </UserInputContainer>
                 </LoginFormSingleInputContainer>
-              )}
-              <LoginFormSingleInputContainer>
-                <GreyTextMedium as="label" htmlFor="userEmail" isLabel>
-                  Email
-                </GreyTextMedium>
-                <UserInputContainer>
-                  <LoginFormInput
-                    id="userEmail"
-                    type="email"
-                    name="userEmail"
-                    value={formUserInput.userEmail}
-                    placeholder="Enter your valid email address"
-                    onChange={onFormUserInputChange}
-                  />
-                </UserInputContainer>
-              </LoginFormSingleInputContainer>
 
-              <LoginFormSingleInputContainer>
-                <GreyTextMedium as="label" htmlFor="userPassword" isLabel>
-                  Password
-                </GreyTextMedium>
-                <UserInputContainer>
-                  <LoginFormInput
-                    id="userPassword"
-                    type={showPassword ? "text" : "password"}
-                    name="userPassword"
-                    value={formUserInput.userPassword}
-                    placeholder="Choose a strong password"
-                    onChange={onFormUserInputChange}
-                    isTypePassword
-                  />
-                  <ToggleShowPasswordButton
-                    type="button"
-                    onClick={onToggleShowPassword}
-                  >
-                    <IconContext.Provider
-                      value={{
-                        style: {
-                          height: "1.5rem",
-                          width: "1.5rem",
-                          color: "#3d3a38",
-                        },
-                      }}
+                <LoginFormSingleInputContainer>
+                  <GreyTextMedium as="label" htmlFor="userPassword" isLabel>
+                    Password
+                  </GreyTextMedium>
+                  <UserInputContainer>
+                    <LoginFormInput
+                      id="userPassword"
+                      type={showPassword ? "text" : "password"}
+                      name="userPassword"
+                      value={formUserInput.userPassword}
+                      placeholder="Choose a strong password"
+                      onChange={onFormUserInputChange}
+                      isTypePassword
+                    />
+                    <ToggleShowPasswordButton
+                      type="button"
+                      onClick={onToggleShowPassword}
                     >
-                      {showPassword ? (
-                        <BsFillEyeFill />
-                      ) : (
-                        <BsFillEyeSlashFill />
-                      )}
-                    </IconContext.Provider>
-                  </ToggleShowPasswordButton>
-                </UserInputContainer>
-              </LoginFormSingleInputContainer>
-            </LoginFormInputsContainer>
+                      <IconContext.Provider
+                        value={{
+                          style: {
+                            height: "1.5rem",
+                            width: "1.5rem",
+                            color: "#3d3a38",
+                          },
+                        }}
+                      >
+                        {showPassword ? (
+                          <BsFillEyeFill />
+                        ) : (
+                          <BsFillEyeSlashFill />
+                        )}
+                      </IconContext.Provider>
+                    </ToggleShowPasswordButton>
+                  </UserInputContainer>
+                </LoginFormSingleInputContainer>
+              </LoginFormInputsContainer>
 
-            <LoginFormButton type="submit">Submit</LoginFormButton>
-
-            <LoginFormHorizontalLine />
-
-            <SignupContainer>
-              <SignupMessage>
-                {isLogin ? "New user ?" : "Already signed up ?"}
-              </SignupMessage>
-              <LoginFormButton type="button" onClick={toggleSignup} isOutline>
-                {isLogin ? "Signup" : "Login"}
+              <LoginFormButton
+                type="submit"
+                disabled={authStatus === "initial" ? false : true}
+              >
+                {getSubmitButtonText()}
               </LoginFormButton>
-            </SignupContainer>
-          </LoginFormContainer>
-        </LoginFormContentContainer>
-      </LoginContentContainer>
-    </LoginBgContainer>
-  );
+
+              <LoginFormHorizontalLine />
+
+              <SignupContainer>
+                <SignupMessage>
+                  {isLogin ? "New user ?" : "Already signed up ?"}
+                </SignupMessage>
+                <LoginFormButton type="button" onClick={toggleSignup} isOutline>
+                  {isLogin ? "Signup" : "Login"}
+                </LoginFormButton>
+              </SignupContainer>
+            </LoginFormContainer>
+          </LoginFormContentContainer>
+        </LoginContentContainer>
+      </LoginBgContainer>
+    );
+  }
+
+  return renderedFinalUI;
 };
 
 export default Login;
