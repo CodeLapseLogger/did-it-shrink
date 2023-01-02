0d52279a08186334e1c62695ad39f5de8f230ada
Surya T N <suryatej.nim@yahoo.com>
2023-01-02 16:15:51 +0530

    feat(header): implemented styled components
    Implemented the styled components to render
    the JSX elements with intended CSS styling,
    in the common Header component.
    
    Initial implementation and needs further tuning
    or code refactoring.

---
+++ src/components/common-components/Header/styledComponents.js
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+import { Navigate } from "react-router-dom";
+
+export const HeaderBgContainer = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 1.5rem 2rem;
+  background-color: #eacd3c;
+`;
+
+export const BrandNameButton = styled.button`
+  border: none;
+  background: transparent;
+  outline: none;
+  cursor: pointer;
+`;
+
+export const BrandName = styled.h1`
+  font-family: "Kalam";
+  font-size: 3.5rem;
+  font-weight: 700;
+  color: #f47622;
+`;
+
+export const BrandNamePartial = styled(BrandName)`
+  font-weight: 300;
+`;
+
+export const HeaderMenuAndAccountContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const HeaderMenuContainer = styled.ul`
+  list-style-type: none;
+  padding-left: none;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderMenuItem = styled.li`
+  margin: 0 0.5rem;
+  background-color: ${(props) =>
+    props.isSelected ? "#ffffff" : "transparent"};
+  color: ${(props) => (props.isSelected ? "#eacd3c" : "#ffffff")};
+  flex: auto;
+`;
+
+export const StyledNavigateComponent = styled(Navigate)`
+  font-family: "Montserrat";
+  font-size: 2rem;
+  font-weight: 500;
+  color: #ffffff;
+`;
+
+export const AccountButton = styled.button`
+  border: none;
+  border-left: 0.1rem solid #ffffff;
+  outline: none;
+  cursor: pointer;
+  background: transparent;
+`;
