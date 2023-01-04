5af9fc4b7e5897afc356d1d341169f25ddab4acf
Surya T N <suryatej.nim@yahoo.com>
2023-01-04 22:21:55 +0530

    refactor(App): added context component
    Updated App component to manage state of
    selected navigation link id, current app
    theme chosen by user through their setters
    and the context with AppContext.Provider
    component with 'value' prop being passed
    with the state and the list of navigation
    link data.
    
    This is the way to manage context data at
    the root of the component tree, triggered
    with the App component render through a
    state change in other parts of the app.
    
    It also allows access to context in child
    components without having to pass on data
    through props.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,61 @@
+import { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
+
+import AppContext from "./contexts/AppContext";
 
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Contribute from "./components/Contribute";
 import NotFound from "./components/NotFound";
 
+const navLinkDataList = [
+  {
+    id: "home",
+    name: "Home",
+    navRoute: "/",
+  },
+  {
+    id: "contribute",
+    name: "Contribute",
+    navRoute: "contribute",
+  },
+  {
+    id: "leverage",
+    name: "Leverage",
+    navRoute: "leverage",
+  },
+  {
+    id: "resources",
+    name: "Resources",
+    navRoute: "resources",
+  },
+];
+
 function App() {
+  const [selectedNavLinkId, setSelectedNavLinkId] = useState(
+    navLinkDataList[0].id
+  );
+  const [isLightTheme, toggleIsLightTheme] = useState(true);
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="login" element={<Login />} />
-      <Route path="contribute" element={<Contribute />} />
-      <Route path="not-found" element={<NotFound />} />
-      <Route render={() => <Navigate to="not-found" />} />
-    </Routes>
+    <AppContext.Provider
+      value={{
+        navLinkDataList,
+        selectedNavLinkId,
+        setSelectedNavLinkId,
+        isLightTheme,
+        toggleIsLightTheme,
+      }}
+    >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="contribute" element={<Contribute />} />
+        <Route path="not-found" element={<NotFound />} />
+        <Route render={() => <Navigate to="not-found" />} />
+      </Routes>
+    </AppContext.Provider>
   );
 }
 
