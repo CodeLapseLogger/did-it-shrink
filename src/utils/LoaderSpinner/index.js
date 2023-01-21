508813bd512104305f7f95cc7d1709bcc202f243
Surya T N <suryatej.nim@yahoo.com>
2023-01-21 12:29:14 +0530

    refactor: loader spinner utility
    Created new utility folder: LoaderSpinner,
    to include default configurations for any
    loader spinners used in the app, imported
    from react-loader-spinner npm package.
    
    This commit includes code that sets the
    default configuration for Oval loader spinner,
    which will be exported in any component that
    imports the LoaderSpinner utility. The named
    import of the default config can then be used
    in an instance of the react loader spinner
    component, to set it up for rendering.

---
+++ src/utils/LoaderSpinner/index.js
@@ -0,0 +1,12 @@
+export const defaultOvalSpinnerConfig = {
+  height: 80,
+  width: 80,
+  color: "#eacd3c",
+  wrapperStyle: {},
+  wrapperClass: "",
+  visible: true,
+  ariaLabel: "oval-loading",
+  secondaryColor: "#f5e17e",
+  strokeWidth: 2,
+  strokeWidthSecondary: 2,
+};
