d0dfb1bd355b949cc316dece7539cd52b0963def
Surya T N <suryatej.nim@yahoo.com>
2023-01-29 22:50:07 +0530

    styles(footer): added styled components
    Added styled components to render UI
    elements in Footer component

--- src/components/common-components/Footer/styledComponents.js
+++ src/components/common-components/Footer/styledComponents.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const FooterBgContainer = styled.div`
   display: flex;
@@ -7,4 +8,28 @@ export const FooterBgContainer = styled.div`
   background-color: #eacd3c;
   width: 100%;
   height: 9vh;
+  padding: 1rem 2rem;
+`;
+
+export const FooterText = styled.p`
+  font-family: "Montserrat";
+  font-size: 1.2rem;
+  font-weight: 500;
+  color: #ffffff;
+`;
+
+export const FooterNavLinksList = styled.ul`
+  list-style-type: none;
+  padding-left: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const FooterNavLinkItem = styled.li`
+  margin: 0.5rem 0;
+`;
+
+export const StyledLink = styled(Link, FooterText)`
+  text-decoration: none;
 `;
