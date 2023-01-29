3349373341484489f4e96aade775623096d4bf79
Surya T N <suryatej.nim@yahoo.com>
2023-01-29 23:20:45 +0530

    feat(footer): added jsx element tree
    Added instances of styled components in
    Footer component to create the JSX element
    tree that renders the intended UI.
    
    The AppContext has been used to access the
    navLinkData and isLightTheme data, to render
    nav links in the Footer component. Logic to
    toggle styles between light and dark modes
    is yet to be implemented.

--- src/components/common-components/Footer/index.js
+++ src/components/common-components/Footer/index.js
@@ -1,5 +1,63 @@
-import { FooterBgContainer } from "./styledComponents";
+import {
+  FooterBgContainer,
+  FooterNavLinksList,
+  FooterNavLinkItem,
+  FooterText,
+  StyledLink,
+} from "./styledComponents";
 
-const Footer = (props) => <FooterBgContainer>Page Footer</FooterBgContainer>;
+import {
+  BrandName,
+  BrandNamePartial,
+} from "../../common-styled-components/commonStyledComponents";
+
+import { IconContext } from "react-icons";
+import { MdOutlineCopyright } from "react-icons/md";
+
+import AppContext from "../../../contexts/AppContext";
+
+const Footer = (props) => {
+  return (
+    <AppContext.Consumer>
+      {(appContextData) => {
+        const { navLinkData, isLightTheme } = appContextData;
+
+        return (
+          <FooterBgContainer>
+            <BrandName>
+              Did It <BrandNamePartial>Shrink</BrandNamePartial>
+            </BrandName>
+            <FooterText>
+              Did It Shrink
+              <IconContext.Provider
+                value={{
+                  style: {
+                    height: "1.5rem",
+                    width: "1.5rem",
+                    color: "#ffffff",
+                  },
+                }}
+              >
+                <MdOutlineCopyright />
+              </IconContext.Provider>
+              2023. All Rights Reserved.
+            </FooterText>
+            <FooterNavLinksList>
+              {Object.values(navLinkData).map((navLinkDataItem) => {
+                const { id, name, navRoute } = navLinkDataItem;
+
+                return (
+                  <FooterNavLinkItem key={id}>
+                    <StyledLink to={navRoute}>{name}</StyledLink>
+                  </FooterNavLinkItem>
+                );
+              })}
+            </FooterNavLinksList>
+          </FooterBgContainer>
+        );
+      }}
+    </AppContext.Consumer>
+  );
+};
 
 export default Footer;
