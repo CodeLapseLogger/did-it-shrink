3b73becd693ae05e55bbf75744b158a84b5bd8cc
Surya T N <suryatej.nim@yahoo.com>
2023-01-30 22:51:27 +0530

    styles(footer): added styled component
    Added styled component: IconContainer, to
    have the copy right react icon as its child
    and align to render the intended UI

--- src/components/common-components/Footer/styledComponents.js
+++ src/components/common-components/Footer/styledComponents.js
@@ -40,3 +40,9 @@ export const StyledNavLink = styled(StyledLink)`
   font-weight: 500;
   color: #ffffff;
 `;
+
+export const IconContainer = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
