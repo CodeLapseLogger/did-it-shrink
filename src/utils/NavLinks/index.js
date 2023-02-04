23829cedaf4146dfa93fd887526bd00683af21e2
Surya T N <suryatej.nim@yahoo.com>
2023-02-04 12:36:07 +0530

    feat: add utility for nav link related app flow
    Added new utility module with helper function
    to determine user authentication state and
    accordingly determine the nav link data to
    be used to render JSX elements in App, Header
    and Footer components.
    
    This also helps with reuse of code/data for
    new app features or extensions in the future,
    enabling maintainability of increased complex
    code base.

---
+++ src/utils/NavLinks/index.js
@@ -0,0 +1,52 @@
+import Cookies from "js-cookie";
+
+const authenticatedPageNavLinkData = {
+  home: {
+    id: "home",
+    name: "Home",
+    navRoute: "/",
+  },
+  contribute: {
+    id: "contribute",
+    name: "Contribute",
+    navRoute: "/contribute",
+  },
+  leverage: {
+    id: "leverage",
+    name: "Leverage",
+    navRoute: "/leverage",
+  },
+  resources: {
+    id: "resources",
+    name: "Resources",
+    navRoute: "/resources",
+  },
+};
+
+const unauthenticatedPageNavLinkData = {
+  home: {
+    id: "home",
+    name: "Home",
+    navRoute: "/",
+  },
+  login: {
+    id: "login",
+    name: "Login",
+    navRoute: "/login",
+  },
+  joinNow: {
+    id: "joinNow",
+    name: "Join Now",
+    navRoute: "/join-now",
+  },
+};
+
+export const getPageNavLinkData = () => {
+  const jwt = Cookies.get("did-it-shrink-jwt-token");
+  const pageNavLinkData =
+    jwt === undefined
+      ? unauthenticatedPageNavLinkData
+      : authenticatedPageNavLinkData;
+
+  return pageNavLinkData;
+};
