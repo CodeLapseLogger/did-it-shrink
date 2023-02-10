c0999c115012131f130d7af26b7fe390ec362e70
Surya T N <suryatej.nim@yahoo.com>
2023-02-10 14:15:59 +0530

    feat(home): add section content
    Added styled components to render JSX elements
    with content for different sections in Home
    component.
    
    Implemented JSX element tree in Home component
    to render the intended sections in page body
    and content in top section.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,91 @@
-const Home = (props) => <h1>Home Page</h1>;
+import { useNavigate } from "react-router-dom";
+import { isAuthenticated } from "../../utils/Authentication";
+import { imageData } from "../../assets/images/ImageData";
+
+import {
+  CallToActionButton,
+  HomeBgContainer,
+  HomeSectionContainer1,
+  HomeSectionContainer2,
+  HomeSectionContent,
+  HomeSectionHeader1,
+  HomeSectionHeader2,
+  HomeSectionImageContainer,
+  HomeSectionImageAttributionContainer,
+  HomeSectionImageAttributionContent,
+  HomeSectionText2,
+  TopSectionSubHeaderContainer,
+  TopSectionSubHeader1,
+  TopSectionSubHeader2,
+} from "./styledComponents";
+
+const Home = (props) => {
+  const navigate = useNavigate();
+
+  const onJoinNow = (joinNowEvent) => {
+    joinNowEvent.preventDefault();
+    navigate("/join-now");
+  };
+
+  const onContribute = (contributeEvent) => {
+    contributeEvent.preventDefault();
+    navigate(isAuthenticated() ? "/contribute" : "/join-now");
+  };
+
+  const getImageAttributionContent = (imageData) =>
+    imageData.imageAttributionPreText +
+    imageData.imageAttributionLink +
+    imageData.imageAttributionPostText;
+
+  return (
+    <HomeBgContainer>
+      <HomeSectionContainer1 sectionHeight="60vh">
+        <HomeSectionContent>
+          <HomeSectionHeader1>
+            CROWD SOURCING PLATFORM FOR PRODUCT DATA
+          </HomeSectionHeader1>
+          <TopSectionSubHeaderContainer>
+            <TopSectionSubHeader1>Why Bother ?</TopSectionSubHeader1>
+          </TopSectionSubHeaderContainer>
+          <TopSectionSubHeaderContainer>
+            <TopSectionSubHeader2>Because you paid for it</TopSectionSubHeader2>
+          </TopSectionSubHeaderContainer>
+          <CallToActionButton type="button" onClick={onJoinNow}>
+            JOIN NOW
+          </CallToActionButton>
+        </HomeSectionContent>
+        <HomeSectionImageContainer
+          bgImageUrl={imageData.home.topSection.bgImageUrl}
+        >
+          <HomeSectionImageAttributionContainer>
+            <HomeSectionImageAttributionContent
+              textColor={imageData.home.topSection.imageAttributionTextColor}
+            >
+              {imageData.home.topSection.imageAttributionPreText}
+            </HomeSectionImageAttributionContent>
+            <HomeSectionImageAttributionContent
+              textColor={imageData.home.topSection.imageAttributionTextColor}
+            >
+              {imageData.home.topSection.imageAttributionLink}
+            </HomeSectionImageAttributionContent>
+            <HomeSectionImageAttributionContent
+              textColor={imageData.home.topSection.imageAttributionTextColor}
+            >
+              {imageData.home.topSection.imageAttributionPostText}
+            </HomeSectionImageAttributionContent>
+          </HomeSectionImageAttributionContainer>
+        </HomeSectionImageContainer>
+      </HomeSectionContainer1>
+      <HomeSectionContainer2>
+        <HomeSectionContent></HomeSectionContent>
+        <HomeSectionImageContainer></HomeSectionImageContainer>
+      </HomeSectionContainer2>
+      <HomeSectionContainer1>
+        <HomeSectionContent></HomeSectionContent>
+        <HomeSectionImageContainer></HomeSectionImageContainer>
+      </HomeSectionContainer1>
+    </HomeBgContainer>
+  );
+};
 
 export default Home;
--- src/components/Home/styledComponents.js
+++ src/components/Home/styledComponents.js
@@ -4,12 +4,15 @@ export const HomeBgContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
+  margin: 0;
 `;
 
 export const HomeSectionContainer1 = styled.section`
   background-color: #f5e482;
   display: flex;
   justify-content: center;
+  padding: 1.5rem;
+  height: ${(props) => props.sectionHeight};
 `;
 
 export const HomeSectionContainer2 = styled(HomeSectionContainer1)`
@@ -17,37 +20,74 @@ export const HomeSectionContainer2 = styled(HomeSectionContainer1)`
 `;
 
 export const HomeSectionContent = styled.div`
-  width: 50%;
+  width: 60%;
   display: flex;
   flex-direction: column;
-  margin: 0.5rem;
+  justify-content: center;
+  margin: 1rem;
 `;
 
 export const HomeSectionImageContainer = styled(HomeSectionContent)`
-  justify-content: center;
+  width: 40%;
+  background-image: url(${(props) => props.bgImageUrl});
+  background-size: 90% 100%;
+  background-color: inherit;
+  background-position: center;
+  background-repeat: no-repeat;
+  justify-content: flex-end;
+  /* padding: 1rem; */
 `;
+
+export const HomeSectionImageAttributionContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 0.25rem;
+`;
+
+export const HomeSectionImageAttributionContent = styled.span`
+  color: ${(props) => props.textColor};
+  margin: 0 0 0 0.5rem;
+
+  & a {
+    text-decoration: none;
+    font-family: "Montserrat";
+    font-weight: 1.2rem;
+    color: inherit;
+  }
+`;
+
 export const HomeSectionHeader1 = styled.h1`
   color: #323131;
   font-family: "Montserrat";
   font-size: 2.5rem;
   font-weight: 600;
   margin-bottom: 0.5rem;
+  text-align: start;
 `;
 
 export const HomeSectionHeader2 = styled(HomeSectionHeader1)`
   color: #ffffff;
 `;
 
+export const TopSectionSubHeaderContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 0;
+  width: 100%;
+`;
+
 export const TopSectionSubHeader1 = styled.p`
   color: #d7793e;
   font-family: "Montserrat";
-  font-size: 1.5rem;
+  font-size: 2rem;
   font-weight: 500;
   margin-bottom: 0.5rem;
 `;
 
 export const TopSectionSubHeader2 = styled(TopSectionSubHeader1)`
-  color: #34d5f5;
+  color: #65b22a;
+  margin-top: 0;
 `;
 
 export const HomeSectionText2 = styled.p`
@@ -68,4 +108,6 @@ export const CallToActionButton = styled.button`
   color: #ffffff;
   cursor: pointer;
   outline: none;
+  align-self: flex-start;
+  margin-top: 0.5rem;
 `;
